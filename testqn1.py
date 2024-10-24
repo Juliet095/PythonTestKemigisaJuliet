ccf2ae28f6aa707c04cbbc25322eb4026379f9ff
Juliet095 <julike2002a@gmail.com>
2024-10-24 11:11:47 +0300

    Completed Python Test

---
+++ testqn1.py
@@ -0,0 +1,18 @@
+# Question 1(i)
+# Write a Python program to find the distance between two coordinate points (x1, y1) and (x2, y2).
+import math
+distance =math .sqrt()
+
+
+
+
+
+
+
+
+# Question 1(ii)
+# Write a Python program to find maximum between three numbers.
+def maximum_between_three_numbers():
+    number =( 1 , 5 , 8 )
+    for x in number
+    print(x)
---
+++ testqn3.py
@@ -0,0 +1,24 @@
+# Question 3(i)
+#  Write a Python program that prompts a user to enter numbers. The process will repeat until
+#  the user enters 0. Finally, the program prints sum of the numbers entered by the user.
+
+numbers = int(input("Enter numbers"))
+print(numbers)
+
+sum = 0
+total_sum = (sum+numbers)
+print (total_sum)
+
+    
+    
+
+
+
+
+
+
+
+
+# Question 3(ii)
+# Write a Python program to print all the numbers from 1 to 100 that are not divisible by 2
+    
